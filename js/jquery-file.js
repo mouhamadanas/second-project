89fcfe290e78a670bb2b83f1135fa3d8d0f82b2c
mouhamadanas <anasalshekhabdalrahman@gmail.com>
2021-04-23 16:25:12 +0300

    finished

--- js/jquery-file.js
+++ js/jquery-file.js
@@ -15,16 +15,48 @@ $(function () {
             "transform": "rotateZ(360deg)"
         });
     });
+    $(".img-2").click(function () {
+        $("header .container1 .cont-links1 .cont-links2>ul").css({
+            "display": "inline",
+            "flex-direction": "column",
+            "position": "absolute",
+            "color": "black",
+            "background-color": "white",
+            "margin": "133px 30px auto 30px",
+            "width": "74%",
+            "text-align": "center",
+            "height": "550px"
+        });
+        $("header .container1 .cont-links1 .cont-links2").css({
+            "flex-direction": "column",
+        });
+        $("header .container1 .cont-links1 .cont-links2 ul>li").css({
+            "padding": "20px",
+            "font-size": "22px",
+            "font-weight": "600"
+        });
+        $("header .container1 .cont-links1 .cont-links2 ul li img").attr("src", "images/icon-arrow-dark.svg");
+        $(".img-2").css("display", "none");
+        $(".img-3").css("display", "inline");
+        $(".cont-btn2").css({
+            "display": "flex",
 
-    $(window).on("resize",function(){
-        if($(window).width()<=500)
-        {
-            $("header .container1 .cont-links1 .cont-links2 ul ").css("display","none");
-            $("header .container1 .cont-btn").css("display","none");
-            $("header .container1 .cont-links1").append("<img src='images/icon-hamburger.svg' alt=''>");
-        }
+
+
+
+
+        });
+    });
+    $(".img-3").on("click", function () {
+        $("header .container1 .cont-links1 .cont-links2 ul").css({
+            "display": "none"
+        });
+        $(".img-3").css("display", "none");
+        $(".img-2").css("display", "inline");
+
+
     });
 
-    
+
 
 });
